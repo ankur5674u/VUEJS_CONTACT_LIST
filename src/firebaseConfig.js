2beaa991446c3e7ecb9335fe6f820226441f48fa
Ankur Prakash Singh <ankur5674u@gmail.com>
2018-05-02 23:08:06 +0530

    First commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,8 +1,11 @@
+/**
+ * Please update firbase config object from firbase project console.
+ */
 export default{
-    apiKey: "AIzaSyDS238vKA5bmjf1AGAaXpAPHG9ZQ8g-B1M",
-    authDomain: "vuejs-contact-list.firebaseapp.com",
-    databaseURL: "https://vuejs-contact-list.firebaseio.com",
-    projectId: "vuejs-contact-list",
-    storageBucket: "vuejs-contact-list.appspot.com",
-    messagingSenderId: "1069957270726"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 }
